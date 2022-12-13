7ee2a0681bcad89906b3105c001c5cd271efc1ee
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-12-13 16:22:33 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('#name').keyup(function (e) {
+        $('.card-name').text($(this).val());
+    });
+    $('#cvc').keyup(function (e) { 
+        $('.cvc-value').text($(this).val());
+    });
+});
+
+// function isNumber(num) {
+//     if (typeof())
+// }
