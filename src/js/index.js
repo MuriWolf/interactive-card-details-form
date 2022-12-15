ec994419df8e76d91f2912cfa5afc242e97ab590
Murillo Pinheiro de Oliveria <83728277+MuriWolf@users.noreply.github.com>
2022-12-14 21:01:57 -0300

    j

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
+    console.log($('.card-number').text().length);
     $('#name').keyup(function (e) {
         $('.card-name').text($(this).val());
     });
     $('#cvc').keyup(function (e) { 
         $('.cvc-value').text($(this).val());
     });
+
+    $('#expMonth').keyup(function (e) { 
+        $('.cvc-value').text($(this).val());
+    });
+    $('#expYear').keyup(function (e) { 
+        $('.cvc-value').text($(this).val());
+    });
+
+
+    $('#cardNumber').keyup(function (e) { 
+        $('.card-number').text($(this).val());
+    });
+
+
 });
